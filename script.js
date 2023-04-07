bf79435487ac45215fb8e0a16812852be678716d
Quillinan <milton.quillinan1798@gmail.com>
2023-04-07 19:48:43 -0300

    add function-SortCardsBack

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let input_cards = parseInt(prompt('Com quantas cartas você quer jogar?'));
 let row = document.querySelector('.rowCards');
 
+let gifs = [
+  'bobrossparrot.gif',
+  'explodyparrot.gif',
+  'fiestaparrot.gif',
+  'metalparrot.gif',
+  'revertiparrot.gif',
+  'tripletsparrot.gif',
+  'unicornparrot.gif',
+];
+
+gifs.sort(comparador);
+
+let gifsSort = gifs.slice(0, input_cards);
+
+gifsSort.sort(comparador);
+
 for (let i = 0; i < input_cards; i++) {
   row.innerHTML =
     row.innerHTML +
@@ -12,9 +28,13 @@ for (let i = 0; i < input_cards; i++) {
      </div>
 
      <div class="back-face face">
-      <img data-test="face-down-image" src="projeto__parrots__imagens/Arquivos Úteis - Projeto 04 - Parrot Card Game/bobrossparrot.gif" />
+      <img data-test="face-down-image" src="projeto__parrots__imagens/Arquivos Úteis - Projeto 04 - Parrot Card Game/${gifsSort[i]}" />
      </div>
 
   </div>
   `;
+}
+
+function comparador() {
+  return Math.random() - 0.5;
 }
